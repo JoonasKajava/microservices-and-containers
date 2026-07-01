a1a3cf981e1bbdd97680be01eadb17862b2ab38b
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-07-01 16:21:15 +0300

    feat: very initial zmq request response

--- inventory/src/main.rs
+++ inventory/src/main.rs
@@ -1,3 +1,31 @@
+use serde::Serialize;
+use zmq::{Context, Message, SocketType::REP};
+
 fn main() {
-    println!("Hello, world!");
+    let context = Context::new();
+    let responder = context.socket(REP).unwrap();
+
+    responder.bind("tcp://*:5555").unwrap();
+
+    let mut msg = Message::new();
+    loop {
+        responder.recv(&mut msg, 0).unwrap();
+
+        println!("Received request: {}", msg.as_str().unwrap());
+
+        let equipment = Equipment {
+            id: 1,
+            name: "Sword".to_string(),
+        };
+
+        let response = serde_json::to_string(&equipment).unwrap();
+
+        responder.send(&response, 0).unwrap();
+    }
+}
+
+#[derive(Serialize)]
+struct Equipment {
+    id: usize,
+    name: String,
 }
--- reservation/src/main.rs
+++ reservation/src/main.rs
@@ -1,3 +1,25 @@
+use std::thread;
+
+use zmq::{Context, Message, SocketType::REQ};
+
 fn main() {
-    println!("Hello, world!");
+    let context = Context::new();
+
+    let requester = context.socket(REQ).unwrap();
+
+    requester.connect("tcp://localhost:5555").unwrap();
+
+    let mut msg = Message::new();
+
+    loop {
+        println!("Sending request...");
+
+        requester.send("Hello", 0).unwrap();
+
+        requester.recv(&mut msg, 0).unwrap();
+
+        println!("Received reply: {}", msg.as_str().unwrap());
+
+        thread::sleep(std::time::Duration::from_secs(1));
+    }
 }
