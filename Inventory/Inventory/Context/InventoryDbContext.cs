e05da940793158bd8e90bf05bedad6b69ad7065e
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2026-07-18 23:28:38 +0300

    Scaffold db context

---
+++ Inventory/Inventory/Context/InventoryDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Context;
+
+public partial class InventoryDbContext : DbContext
+{
+    public InventoryDbContext()
+    {
+    }
+
+    public InventoryDbContext(DbContextOptions<InventoryDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=equipment_reservation;Username=admin;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
